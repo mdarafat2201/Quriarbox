943f1d3b277f2302f9759f5d60d35391392022b2
mdarafat2201 <maccenarafat@gmail.com>
2024-02-04 18:08:42 +0000

    card3

--- src/Component/CommonComponent/Card.jsx
+++ src/Component/CommonComponent/Card.jsx
@@ -1,23 +1,32 @@
 import React from "react";
 import Button from "../CommonComponent/Button";
+import { IoBagHandleOutline } from "react-icons/io5";
 
 const Card = () => {
   return (
     <>
-      <div className="w-1/3 bg-white p-10">
-        <h1>Business Services</h1>
-        <p>
-          We give you complete reliable delivery for your company. We will take
-          full responsibility of the deliveries.
-        </p>
-        <ul>
-          <li>Corporate goods</li>
-          <li>Shipment</li>
-          <li>Accesories</li>
-        </ul>
-        <Button className="border-2 p-5 border-orangecolor rounded-md text-orangecolor">
-          Learn more
-        </Button>
+      <div className="mt-10">
+        <div className="w-[40%] bg-white p-10">
+          <div className="text-center">
+            <span>
+              <IoBagHandleOutline className="inline-block text-5xl text-orangecolor" />
+            </span>
+            <h1>Business Services</h1>
+          </div>
+
+          <p>
+            We give you complete reliable delivery for your company. We will
+            take full responsibility of the deliveries.
+          </p>
+          <ul>
+            <li>Corporate goods</li>
+            <li>Shipment</li>
+            <li>Accesories</li>
+          </ul>
+          <Button className="border-2 p-5 border-orangecolor rounded-md text-orangecolor">
+            Learn more
+          </Button>
+        </div>
       </div>
     </>
   );
